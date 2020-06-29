ab2096f212b8889df6f3004fb2029ba61bac1420
José Luis GA <josto90@hotmail.com>
2020-06-29 12:23:52 +0200

    Update Funciones.py

--- Apuntes/Funciones.py
+++ Apuntes/Funciones.py
@@ -26,3 +26,39 @@ def mi_funcion(*niños):
 mi_funcion("Emil", "Tobias", "Linus") 
 
 print()
+
+# Argumentos pasados como clave:valor
+def mi_funcion(niño3, niño2, niño1):
+  print("El niño mas joven es " + niño3)
+
+mi_funcion(niño1 = "Emil", niño2 = "Tobias", niño3 = "Linus") 
+
+print()
+
+# Argumentos pasados como clave:valor desconocidos
+def mi_funcion(**niño):
+  print("Su apellido es " + niño["lname"])
+
+mi_funcion(fname = "Tobias", lname = "Refsnes") 
+
+print()
+
+# Devolver valores
+
+def mi_funcion(x):
+  return 5 * x
+
+# Pasar para que no de error
+def mi_funcion():
+  pass
+
+# Recursión
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+tri_recursion(6)

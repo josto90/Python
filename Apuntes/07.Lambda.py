eceb49b54ccfa360af5fa7b71aa1ead6114d584a
José Luis GA <josto90@hotmail.com>
2020-06-29 12:40:07 +0200

    Update 07.Lambda.py

--- Apuntes/07.Lambda.py
+++ Apuntes/07.Lambda.py
@@ -1,3 +1,6 @@
 ############
 ## LAMBDA ##
 ############
+
+# Una función lambda es una pequeña función anónima.
+# Una función lambda puede tomar cualquier número de argumentos, pero solo puede tener una expresión.

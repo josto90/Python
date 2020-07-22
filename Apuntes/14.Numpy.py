16591a2787db44ead54bd5d1e5710d7c379a679b
José Luis GA <josto90@hotmail.com>
2020-07-22 10:23:03 +0200

    Create 14.Numpy.py

---
+++ Apuntes/14.Numpy.py
@@ -0,0 +1,9 @@
+###########
+## NUMPY ##
+###########
+
+# NumPy es una biblioteca de Python utilizada para trabajar con matrices.
+# También tiene funciones para trabajar con álgebra lineal.
+
+# En Python tenemos listas que sirven al propósito de las matrices, pero su procesamiento es lento. NumPy proporciona un objeto de matriz 50 veces más rápido.
+# El objeto de matriz en NumPy se llama ndarray, proporciona muchas funciones de soporte que hacen que trabajar con ndarray sea muy fácil.

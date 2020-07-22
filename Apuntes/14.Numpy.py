c3ec839d482b125078b1ecfa8e92b8a50a610369
José Luis GA <josto90@hotmail.com>
2020-07-22 15:15:34 +0200

    Update 14.Numpy.py

--- Apuntes/14.Numpy.py
+++ Apuntes/14.Numpy.py
@@ -7,3 +7,7 @@
 
 # En Python tenemos listas que sirven al propósito de las matrices, pero su procesamiento es lento. NumPy proporciona un objeto de matriz 50 veces más rápido.
 # El objeto de matriz en NumPy se llama ndarray, proporciona muchas funciones de soporte que hacen que trabajar con ndarray sea muy fácil.
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5]) # También se puede con ((1,2,3,4,5))
+print(arr)
+print(type(arr))

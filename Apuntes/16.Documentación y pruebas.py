32ed2a397ed4d3e0fe95a2f5d9cb75b3178b3011
José Luis GA <josto90@hotmail.com>
2020-07-28 10:23:22 +0200

    Update 16.Documentación y pruebas.py

--- Apuntes/16.Documentación y pruebas.py
+++ Apuntes/16.Documentación y pruebas.py
@@ -31,6 +31,40 @@ help(o)
 import requests
 help(requests)
 help(len)
+
+
 ###########
 ## PYDOC ##
 ###########
+
+# Desde la terminal no podemos utilizar la función help, pero sí existe la posibilidad de utilizar pydoc 
+
+### pydoc nombre.py --> Ejecutar en terminal
+# Una función interesante de Pydoc es que podemos generar la documentación de nuestro código en HTML utilizando la siguiente sintaxis:
+
+### pydoc -w nombre.py
+### pydoc -w .\ --> Genera la documentación de forma recursiva de todo un paquete
+
+
+#############
+## DOCTEST ##
+#############
+
+# doctest nos permiten combinar pruebas en la propia documentación.
+# Por regla general cada prueba va ligada a una funcionalidad, pueden ser funciones, clases o sus métodos. Por ejemplo:
+
+def suma(a, b):
+    """Esta función recibe dos parámetros y devuelve la suma de ambos
+
+    >>> suma(5,10)
+    15
+    """
+    return a+b
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+# Así únicamente se lanzarán las pruebas al ejecutar directamente el módulo, y ya podremos ejecutar el módulo desde la terminal:
+
+### python test.py -v

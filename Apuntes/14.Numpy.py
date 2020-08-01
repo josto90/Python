c84209636609ef31c1061be9cf27cdda1bad61b5
José Luis GA <josto90@hotmail.com>
2020-08-01 08:18:07 +0200

    Numpy

--- Apuntes/14.Numpy.py
+++ Apuntes/14.Numpy.py
@@ -40,15 +40,6 @@ print(arr3.ndim)
 
 print()
 
-# Acceso a elementos
-print('Segundo elemento de la primera dimensión: ', arr2[0, 1])
-
-print()
-
-# Muestra del array
-print(arr3[1:5]) 
-
-print()
 
 # Generando arrays
 
@@ -77,3 +68,64 @@ arr0 = np.array(lista1)
 print(arr0)
 
 print()
+
+
+# Operaciones con arrays
+
+import numpy as np
+array1 = np.array([1,2,3,4])
+
+array1 = array1 + 4 # Le suma 4 a cada elemento
+print(array1)
+
+array1 = array1 / 2
+print(array1)
+
+print()
+
+# Indexación arrays
+import numpy as np
+array = np.arange(0,11)
+print(array)
+print(array[0:3])
+
+print()
+
+# Muestra del array
+print(array[1:5]) 
+
+print()
+
+# Acceso a elementos concretos
+print('Segundo elemento de la primera dimensión: ', arr2[0, 1]) # Array definido mas arriba bidimensional
+
+print()
+
+# Mtrices traspuestas(cambio de filas por columnas)
+import numpy as np
+array = np.arange(15).reshape((3,5)) # 3 filas, 5 columnas
+print(array)
+
+array_tras = array.T
+print(array_tras)
+
+# Entrada y salida arrays
+import numpy as np
+array1 = np.arange(6)
+
+np.save('array1s',array1) # Salvamos el array de manera física
+np.load('array1s.npy')
+
+np.savetxt('mificheroaaray.txt',array1,delimiter=',')
+np.loadtxt('mificheroaaray.txt',delimiter=',')
+
+# Funciones con arrays
+import numpy as np
+array1 = np.arange(5)
+array2 = np.arange(5)
+
+print(np.sqrt(array1)) # Raíz cuadrada
+
+print(np.random.rand(5)) # Números random
+
+print(np.add(array1,array2)) # Suma de dos arrays
